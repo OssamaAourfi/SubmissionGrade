58357d5c5d9e5d44288b9efcdca4f1eb08405f36
OssamaAourfi <aourfidev@gmail.com>
2024-02-18 17:15:56 +0100

    RequestPAram

--- GradeSubmission/src/main/java/com/ltp/GradeSubmission/GradeSubmissionController.java
+++ GradeSubmission/src/main/java/com/ltp/GradeSubmission/GradeSubmissionController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,7 +15,8 @@ public class GradeSubmissionController {
 
     List<Grade> studentGrades = new ArrayList<>();
     @GetMapping("/")
-    public String gradeForm(Model model){
+    public String gradeForm(Model model , @RequestParam(required = false) String name){
+        System.out.println(name);
         model.addAttribute("grade",new Grade());
         return "form";
     }
